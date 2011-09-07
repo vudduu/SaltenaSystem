8e2d5a82d8c2c0382bc927091662b308ff4376cc
Hernan Payrumani <hernan.payumani@gmail.com>
2011-09-07 10:52:23 -0400

    added laravel framework

---
+++ application/routes.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is the public API of your application. To add functionality to your
+	| application, you just add to the array located in this file.
+	|
+	| Simply tell Laravel the HTTP verbs and request URIs it should respond to.
+	| You may respond to the GET, POST, PUT, or DELETE verbs. Enjoy the simplicity
+	| and elegance of RESTful routing.
+	|
+	| Here is how to respond to a simple GET request to http://example.com/hello:
+	|
+	|		'GET /hello' => function()
+	|		{
+	|			return 'Hello World!';
+	|		}
+	|
+	| You can even respond to more than one URI:
+	|
+	|		'GET /hello, GET /world' => function()
+	|		{
+	|			return 'Hello World!';
+	|		}
+	|
+	| It's easy to allow URI wildcards using the (:num) or (:any) place-holders:
+	|
+	|		'GET /hello/(:any)' => function($name)
+	|		{
+	|			return "Welcome, $name.";
+	|		}
+	|
+	*/
+
+	'GET /' => function()
+	{
+		return View::make('home.index');
+	},
+
+);
---
+++ application/views/home/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<title>Welcome To Laravel!</title> 
+ 
+	<link href="http://fonts.googleapis.com/css?family=Quattrocento&amp;v1" rel="stylesheet" type="text/css" media="all" />
+	<link href="http://fonts.googleapis.com/css?family=Ubuntu&amp;v1" rel="stylesheet" type="text/css" media="all" />
+	<link href='http://fonts.googleapis.com/css?family=Lobster+Two' rel='stylesheet' type='text/css'>
+
+	<style type="text/css">
+		body {
+			background-color: #eee;
+			color: #6d6d6d;
+			font-family: 'Ubuntu';
+			font-size: 15px;
+		}
+
+		a {
+			color: #7089b3;
+			font-weight: bold;
+			text-decoration: none;
+		}
+
+		h1.laravel {
+			font-family: 'Lobster Two', Helvetica, serif;				
+			font-size: 60px;
+			margin: 0 0 15px -10px;
+			padding: 0;
+			text-shadow: -1px 1px 1px #fff;
+		}
+
+		h2 {
+			font-family: 'Quattrocento', serif;
+			font-size: 30px;
+			margin: 30px 0 0 0;
+			padding: 0;
+			text-shadow: -1px 1px 1px #fff;
+		}
+
+		p {
+			margin: 10px 0 0 0;
+			line-height: 25px;
+		}
+
+		#header {
+			margin: 0 auto;
+			margin-bottom: 15px;
+			margin-top: 20px;
+			width: 80%;
+		}
+
+		.wrapper {
+			background-color: #fff;
+			border-radius: 10px;
+			margin: 0 auto;
+			padding: 10px;
+			width: 80%;
+		}
+
+		.wrapper h2:first-of-type {
+			margin-top: 0;
+		}
+	</style>
+</head> 
+<body>
+	<div id="header">
+		<h1 class="laravel">Laravel</h1>
+	</div>
+
+	<div class="wrapper">
+		<h2>Installation Complete!</h2>
+
+		<p>Ready to dig in? Start building your application in the <strong>application/routes.php</strong> file.</p>
+
+		<p>Need to learn more? Peruse our <a href="http://laravel.com">wonderful documentation</a>.</p>
+	</div>
+</body> 
+</html>
